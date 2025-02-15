f33e01fdf0f9eaf2e5970337cc6654ea182f723f
AppleD0g <user@Users-MacBook-Pro.local>
2025-02-15 22:05:26 +0700

    fixing work experienes

--- components/Experience/experienceList.tsx
+++ components/Experience/experienceList.tsx
@@ -1,7 +1,7 @@
 import { ExperienceTypes } from "@/types/experiences.type";
 import React from "react";
 import Title from "../ui/title";
-
+import { Card } from "@/components/ui/card";
 interface ExperienceListProps {
   experience: ExperienceTypes;
 }
@@ -13,10 +13,33 @@ export default function ExperienceList({ experience }: ExperienceListProps) {
         title={experience.company}
         description={experience.position}
         date={experience.date}
+        type={experience.type}
       />
       <p className="text-sm pt-2.5 text-gray-600/80">
         {experience.description}
       </p>
+
+      {/* Responsibilities */}
+      <div className="flex flex-col pt-2.5">
+        <p className="text-sm text-gray-600/80 font-bold">Responsibilities</p>
+      </div>
+
+      {/* Technology Stacks */}
+      <div className="flex flex-col pt-2.5">
+        <p className="text-sm font-bold">Technology Used</p>
+        <div className="flex flex-row gap-2 flex-wrap py-2.5">
+          {experience.technologyStack.map((technology) => (
+            <Card
+              key={technology}
+              className="justify-center flex flex-row items-center px-3 py-1"
+            >
+              <p className="text-xs ">{technology}</p>
+            </Card>
+          ))}
+        </div>
+      </div>
+
+      {/* Locations and url */}
     </div>
   );
 }
--- components/ui/title.tsx
+++ components/ui/title.tsx
@@ -4,13 +4,17 @@ interface TitleProps {
   title: string;
   description: string;
   date?: string;
+  type?: string;
 }
 
-export default function Title({ title, description, date }: TitleProps) {
+export default function Title({ title, description, date, type }: TitleProps) {
   return (
     <div>
       {date && <p className="text-sm text-gray-600/80">{date}</p>}
-      <h1 className="text-lg font-bold">{title}</h1>
+      <div className="flex flex-row items-center gap-2">
+        <h1 className="text-lg font-bold">{title}</h1>
+        {type && <p className="text-sm text-gray-600/80">{type}</p>}
+      </div>
       <p className="text-sm text-gray-600/80">{description}</p>
     </div>
   );
