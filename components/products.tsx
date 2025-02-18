b27b55e3c40fb5f814a80a3232fa2d7d442fa22c
AppleD0g <user@Users-MacBook-Pro.local>
2025-02-18 23:54:18 +0700

    add motion

--- components/products.tsx
+++ components/products.tsx
@@ -1,17 +1,27 @@
 import React from "react";
-import Header from "./ui/header";
+import { motion } from "framer-motion";
+import Header from "@/components/ui/header";
 import ProductsData from "@/data/products.json";
 import ProductList from "./products/productList";
 
 export default function ProductsComponent() {
   return (
-    <section id="products" className="products-section">
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      id="products"
+      className="products-section"
+    >
       <Header
         title="Products"
         description="There are list of publicly available products that I have been contributing to"
       />
       {ProductsData.map((product, index) => (
-        <div
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: index * 0.2 }}
           key={product.id}
           className={`flex flex-col  ${
             index !== ProductsData.length - 1
@@ -20,8 +30,8 @@ export default function ProductsComponent() {
           }`}
         >
           <ProductList key={product.id} product={product} />
-        </div>
+        </motion.div>
       ))}
-    </section>
+    </motion.section>
   );
 }
