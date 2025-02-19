1f2025f6dae532c7951ad4b7bd175ce6b6f4a1d3
AppleD0g <user@Users-MacBook-Pro.local>
2025-02-19 23:51:35 +0700

    add motion for education

--- components/education.tsx
+++ components/education.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import EducationData from "@/data/education.json";
 import { splitTextWithHighlights } from "@/utils/textHighlighter";
 import Header from "./ui/header";
@@ -28,16 +29,34 @@ const HighlightedText = ({
 
 export default function EducationComponent() {
   return (
-    <section className="education-section">
-      <Header
-        title="Education"
-        description="Here is a detailed list of my educational background and academic qualifications."
-      />
+    <motion.section
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ margin: "-100px" }}
+      transition={{ duration: 0.5 }}
+      className="education-section"
+    >
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ margin: "-100px" }}
+        transition={{ duration: 0.5 }}
+      >
+        <Header
+          title="Education"
+          description="Here is a detailed list of my educational background and academic qualifications."
+        />
+      </motion.div>
 
       {EducationData.map((education, index) => (
-        <div
+        <motion.div
           key={education.id}
-          className={`flex flex-col  ${
+          initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+          viewport={{ margin: "-100px" }}
+          transition={{ duration: 0.5, delay: index * 0.2 }}
+          className={`flex flex-col ${
             index !== EducationData.length - 1
               ? "py-3 md:py-4 lg:py-5"
               : "pt-3 md:pt-4 lg:pt-5"
@@ -54,8 +73,8 @@ export default function EducationComponent() {
               highlights={education.highlights}
             />
           </p>
-        </div>
+        </motion.div>
       ))}
-    </section>
+    </motion.section>
   );
 }
