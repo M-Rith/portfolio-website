229d1579c8c32282f199142116a9c32fe5ad8e51
AppleD0g <user@Users-MacBook-Pro.local>
2025-02-21 00:40:23 +0700

    add-not-found-page

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import notFoundImage from "@/public/notFound.svg";
+import { Button } from "@/components/ui/button";
+export default function NotFound() {
+  return (
+    <div className="flex flex-col h-[95vh] items-center justify-center">
+      <Image src={notFoundImage} alt="404" className="w-1/2 " />
+      <h1 className="text-3xl font-bold">Page Not Found</h1>
+
+      <p className="text-md text-gray-600 pb-5">
+        The page you are looking for does not exist.
+      </p>
+
+      <Button variant="outline" className="text-md text-gray-600">
+        <Link href="/">Back to Home</Link>
+      </Button>
+    </div>
+  );
+}
