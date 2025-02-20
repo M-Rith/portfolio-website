483f19460a8488ca7d58e6fd3aeb61eb243a22d1
AppleD0g <user@Users-MacBook-Pro.local>
2025-02-20 22:54:14 +0700

    fixing issue

--- components/experiences.tsx
+++ components/experiences.tsx
@@ -1,28 +1,48 @@
 import React from "react";
+import { motion } from "framer-motion";
 import Title from "./ui/header";
 import ExperiencesData from "@/data/experienes.json";
 import ExperienceList from "./experiences/experienceList";
 
 export default function ExperienceComponent() {
   return (
-    <div>
-      <Title
-        title="Work Experiences"
-        description="Here are the key responsibilities and tasks I have been handling in my work experience."
-      />
+    <motion.section
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ margin: "-100px" }}
+      transition={{ duration: 0.5 }}
+      id="experiences"
+      className="experiences-section"
+    >
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ margin: "-100px" }}
+        transition={{ duration: 0.5 }}
+      >
+        <Title
+          title="Work Experiences"
+          description="Here are the key responsibilities and tasks I have been handling in my work experience."
+        />
+      </motion.div>
 
       {ExperiencesData.map((experience, index) => (
-        <div
+        <motion.div
+          initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+          viewport={{ margin: "-100px" }}
+          transition={{ duration: 0.5, delay: index * 0.2 }}
           key={experience.id}
-          className={`flex flex-col  ${
+          className={`flex flex-col ${
             index !== ExperiencesData.length - 1
               ? "py-3 md:py-4 lg:py-5"
               : "pt-3 md:pt-4 lg:pt-5"
           }`}
         >
           <ExperienceList key={experience.id} experience={experience} />
-        </div>
+        </motion.div>
       ))}
-    </div>
+    </motion.section>
   );
 }
