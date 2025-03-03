632fb177455f00c3ac0bc847f8658d6276e366da
AppleD0g <user@Users-MacBook-Pro.local>
2025-03-03 15:55:27 +0700

    fixing

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import blogs from "@/data/blogs.json";
+import BlogData from "@/data/blogs.json";
 import BlogCard from "@/components/blogs/blog-card";
 import Header from "@/components/ui/header";
+import { BlogsTypes } from "@/types/blogs.type";
 
 export default function BlogPage() {
   return (
     <div className="flex flex-col min-h-[85vh]">
       <Header title="📝 Blogs" description="Here are some of my blogs" />
-      {blogs.map((blog) => (
-        <div key={blog.id} className="py-3 md:py-4 lg:py-5">
-          <BlogCard key={blog.id} blog={blog} />
-        </div>
-      ))}
+      {BlogData.map((blog) => {
+        return (
+          <div key={blog.id} className="py-3 md:py-4 lg:py-5">
+            <BlogCard blog={blog as BlogsTypes} />
+          </div>
+        );
+      })}
     </div>
   );
 }
